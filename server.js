fedb20bef3a030aed79dfc8348198edddc634abe
KentoMoriwaki <kento@wantedly.com>
2017-02-16 11:30:42 +0900

    Return saved data

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
+var express = require('express');
+var url = require('url');
 var config = require('./webpack.config');
 
-const port = process.env.PORT || '3000'
+var app = express();
+app.get('/data/:data.json', function(req, res) {
+  res.sendFile(`${__dirname}/public/data/${req.params.data}.json`);
+});
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
@@ -11,10 +16,12 @@ new WebpackDevServer(webpack(config), {
   stats: {
     colors: true
   }
-}).listen(port, 'localhost', function (err) {
+}).listen(3000, 'localhost', function (err) {
   if (err) {
     console.log(err);
   }
 
-  console.log(`Listening at localhost:${port}`);
+  console.log(`Listening at localhost:3000`);
 });
+
+app.listen(3001);
